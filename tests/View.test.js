7001e18a91c4271b8f9f2f25cd7e46fbbb37f34e
Dave McVicar <machvee@gmail.com>
2022-08-16 17:15:49 -0400

    View tests each participant and all participants now

--- tests/View.test.js
+++ tests/View.test.js
@@ -53,7 +53,7 @@ test('view can render a single participant', async () => {
 
   await v.renderParticipant(
     chatStar,
-    "tests/fixtures/Grid.pug",
+    "tests/fixtures/views/PrivateGrid.pug",
     chatEvent.context,
     MockXStateEvent(chatStar),
     {
@@ -64,3 +64,53 @@ test('view can render a single participant', async () => {
   expect(spy).toHaveBeenCalledTimes(1)
   expect(spy).toHaveBeenCalledWith(chatStar, html, moveTimeout)
 })
+
+test('view can render to all participant', async () => {
+  const chatEvent = new Chat305Event(chatEventJson, MessagingStub)
+  const spy = jest.spyOn(chatEvent, 'publishEventParticipantsMessage').mockImplementation(async () => null);
+  const lastStar = new ChatParticipant(chatParticipant1Json)
+  const moveTimeout = 140000
+  const v = new View(chatEvent)
+  const html = '<div class="grid"><div class="chat-name">Chat 305</div>'
+  + '<div class="message">you up?</div><div class="status">blue</div></div>'
+
+  await v.renderAllParticipants(
+    "tests/fixtures/views/ParticipantsGrid.pug",
+    chatEvent.context,
+    MockXStateEvent(lastStar),
+    {
+      timeout: moveTimeout,
+      color: "blue"
+    }
+  )
+  expect(spy).toHaveBeenCalledTimes(1)
+  expect(spy).toHaveBeenCalledWith(html, moveTimeout)
+})
+
+test('view can render to each participant privately', async () => {
+  const chatEvent = new Chat305Event(chatEventJson, MessagingStub)
+  const spy = jest.spyOn(chatEvent, 'publishEventPrivateMessage').mockImplementation(async () => null);
+  const v = new View(chatEvent)
+  const lastStar1 = new ChatParticipant(chatParticipant1Json)
+  const lastStar2 = new ChatParticipant(chatParticipant2Json)
+  const html1 = '<div class="grid"><div class="participant-name">machvee</div>' 
+  + '<div class="participant-alias">Para-bebe</div><div class="chat-name">Chat 305</div>'
+  + '<div class="message">you up?</div><div class="status">green</div></div>'
+  const html2 = '<div class="grid"><div class="participant-name">bbondfl93</div>' 
+  + '<div class="participant-alias">Para-bebe</div><div class="chat-name">Chat 305</div>'
+  + '<div class="message">you up?</div><div class="status">green</div></div>'
+
+  await v.renderEachParticipant(
+    [lastStar1, lastStar2],
+    "tests/fixtures/views/PrivateGrid.pug",
+    chatEvent.context,
+    MockXStateEvent(lastStar1),
+    {
+      color: "green"
+    }
+  )
+  expect(spy).toHaveBeenCalledTimes(2)
+  expect(spy).toHaveBeenNthCalledWith(1, lastStar1, html1, 0)
+  expect(spy).toHaveBeenNthCalledWith(2, lastStar2, html2, 0)
+})
+
