82dd78ab3bf5cd2038a469892de3357d2ea1a90a
Dave McVicar <machvee@gmail.com>
2022-11-11 12:35:09 -0500

    DO NOT perist geoLocation

--- tests/AnearParticipant.test.js
+++ tests/AnearParticipant.test.js
@@ -43,3 +43,40 @@ test('participant can be repeatedly rehydrated and updated', async () => {
     console.error(error)
   }
 })
+
+test('participant does not persist geoLocation', async () => {
+  try {
+    const someGeoLocation = {
+      coords: {
+        latitude: 25.7862067,
+        longitude: -80.1415718
+      }
+    }
+    const participant = new TestParticipant(player1, MockEvent)
+
+    participant.geoLocation = someGeoLocation
+
+    await participant.persist()
+
+    expect(participant.geoLocation.coords.latitude).toBe(someGeoLocation.coords.latitude)
+
+    let p = await TestParticipant.getFromStorage(player1.data.id, MockEvent)
+
+    expect(p.geoLocation).toBe(null)
+
+    expect(p.context.responses).toStrictEqual(['A', 'C', 'D', 'A'])
+    expect(p.anearEvent).toBe(MockEvent)
+    p.context.responses.push('B')
+
+    await p.update()
+
+    p = await TestParticipant.getFromStorage(player1.data.id, MockEvent)
+    expect(p.context.responses[4]).toBe('B')
+    expect(p.anearEvent).toBe(MockEvent)
+
+    await p.remove()
+
+  } catch(error) {
+    console.error(error)
+  }
+})
