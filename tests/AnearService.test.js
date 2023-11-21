10365f0181645850c83d73417f1edf2f00cd02f9
Dave McVicar <machvee@gmail.com>
2023-11-21 14:36:46 -0500

    incremental WIP commit only

--- tests/AnearService.test.js
+++ tests/AnearService.test.js
@@ -6,6 +6,6 @@ const AppId = '5b9d9838-17de-4a80-8a64-744c222ba722'
 const MockAppClass = class AppMachine {}
 const MockParticipantClass = class ParticipantMachine {}
 
-test('test happy path', async () =>  {
+test('test happy path', () =>  {
   const service = new AnearService(AppId, MockAppClass, MockParticipantClass)
 })
